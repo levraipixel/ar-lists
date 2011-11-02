1c192234fdb3dfb5a5a1e539a8066f4896b263ee
Yann Hourdel <yann.hourdel@gmail.com>
2011-11-02 16:15:40 +0100

    Added polymorphic storage and related documentation

--- lib/ar-lists/class_methods.rb
+++ lib/ar-lists/class_methods.rb
@@ -7,44 +7,74 @@ module ArLists
 
 	module ClassMethods
     
+    # attr_name	: the attribute
+    # uniq			: wether to act like a group (if true) or like an array (if false)
+    # encode		: encoding function (must return a String)
+    # decode		: decoding function (takes a String as argument)
 		def array_accessible(attr_name, options = {})
 			do_uniq = false || options[:uniq]
 			attr_accessible attr_name
+
+			encode = options.has_key?(:encode) ? options[:encode] : lambda {|v| return v.to_s}
+			decode = options.has_key?(:decode) ? options[:decode] : lambda {|v| return v}
+
 			define_method "#{attr_name}=" do |values|
-				val = [values].flatten.map{|v| v.to_s.split(',')}.flatten.reject{|v| v.blank?}
+				val = [values].flatten.map{|v| encode.call(v).split(',')}.flatten.reject{|v| v.blank?}
 				val.uniq! if do_uniq
 				write_attribute attr_name, val.join(',')
 			end
 			define_method "#{attr_name}" do
 				val = read_attribute attr_name
-				val.blank? ? [] : val.split(',')
+				val.blank? ? [] : decode.call(val).split(',')
 			end
 		end
 
+		# attr_name		: the attribute
+		# uniq				: wether to act like a group (if true) or like an array (if false)
+		# strict			: wether to throw an error when a Record cannot be found
+		# polymorphic	: wether to type the elements (if true) or not (if false)
+		# class_name	: the type to use when polymorphic is false
 		def has_list(attr_name, options = {})
+
 			do_uniq = false || options[:uniq]
 			strict = false || options[:strict]
-			class_name = options[:class_name].blank? ? attr_name.singularize.classify : options[:class_name]
-			attr_accessible attr_name, "#{attr_name}_ids"
-			define_method "#{attr_name}_ids=" do |values|
-				val = [values].flatten.map{|v| v.to_s.split(',')}.flatten.reject{|v| v.blank?}
-				val.uniq! if do_uniq
-				write_attribute attr_name, val.join(',')
+			is_typed = false || options[:polymorphic]
+
+			unless is_typed
+				class_name = options[:class_name].blank? ? attr_name.singularize.classify : options[:class_name]
+				attr_accessible attr_name, "#{attr_name}_ids"
+				define_method "#{attr_name}_ids=" do |values|
+					val = [values].flatten.map{|v| v.to_s.split(',')}.flatten.reject{|v| v.blank?}
+					val.uniq! if do_uniq
+					write_attribute attr_name, val.join(',')
+				end
+				define_method "#{attr_name}_ids" do
+					val = read_attribute attr_name
+					val.blank? ? [] : val.split(',').map{|s| s.to_i}
+				end
 			end
-			define_method "#{attr_name}_ids" do
-				val = read_attribute attr_name
-				val.blank? ? [] : val.split(',').map{|s| s.to_i}
-			end
+
 			define_method "#{attr_name}=" do |records|
-				val = records.map{|r| r.id.to_s}
+				val = records.map{|r|
+					if is_typed
+						"#{r.class}:#{r.id}"
+					else
+						"#{r.id}"
+					end
+				}
 				val.uniq! if do_uniq
 				write_attribute attr_name, val.join(',')
 			end
 			define_method "#{attr_name}" do
 				val = read_attribute attr_name
-				val.blank? ? [] : val.split(',').map{|id|
+				val.blank? ? [] : val.split(',').map{|r_code|
 					begin
-						class_name.constantize.find(id)
+						if is_typed
+							code = r_code.split(':')
+							code[0].classify.constantize.find(code[1].to_i)
+						else
+							class_name.constantize.find(r_code)
+						end
 					rescue RecordNotFound
 						throw "Unable to build list: RecordNotFound" if strict
 						[]
